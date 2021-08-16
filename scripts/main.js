2d8870df3653494c9061f57cdcffb7ed958af909
Pavlo Hadzheha <i.w.gadzhega@gmail.com>
2021-08-16 22:56:53 +0300

    completed change functionality for text color and background

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,9 @@
 'use strict';
 
-// $('#bgFile input').prop('defaultValue', 'Browse')
-// console.dir($('#bgFile input'));
 $(function () {
     const COLORS = [
         "#eaff7b", "#00ffab", "#29bdc1", "#d84242", "#913f92", "#e81676", "#d718cf",
-        "#f065f1", "#874df7", "#6b03c2", "#ae1f6e", "#3b415e", "#74a6cc", "#68d4c7",
+        "#f065f1", "#874df7", "#6b03c2", "#ae1f6e", "#3b415e", "#fff", "#68d4c7",
         "#cce77f", "#d40f0f", "#3a21db", "#0ab02e", "#e7de11", "#7c7c7c", "#74b66f"
     ];
 
@@ -44,12 +42,39 @@ $(function () {
     })
 
     COLORS.forEach(elem => {
-        $('.color-grid').append(`<div class="color-box" style="background-color: ${elem}"></div>`)
+        $('.color-grid').append(`<div class="color-box" value="${elem}" style="background-color: ${elem}"></div>`)
     });
 
     BG_IMAGES.forEach(elem => {
-        $('.image-grid').append(`<div class="image-box" style="background-image: url('${elem}')"></div>`)
+        $('.image-grid').append(`<div class="image-box" value="${elem}" style="background-image: url('${elem}')"></div>`)
     });
+
+    $('.color-grid').click(event => {
+        let trg = $(event.target);
+        if($(trg).is('.color-box')){
+            let value = $(trg).attr('value');
+            if($(trg).parent().is('#bgColors')){
+                $('body').css({
+                    backgroundColor: value,
+                })
+            } else {
+                $('#result').css("color", value)
+            }
+        }
+    })
+
+    $('.image-grid').click(event => {
+        let trg = $(event.target);
+        if($(trg).is('.image-box')){
+            let value = $(trg).attr('value');
+            if($(trg).parent().is('#bgImages')){
+                $('body').css({
+                    backgroundColor: '',
+                    backgroundImage: `url('${value}')`
+                })
+            }
+        }
+    })
 
 
     $(mainForm).click(function(event){
@@ -86,7 +111,7 @@ $(function () {
     })
 
 
-    $(loginForm.password).bluer(function(event){
+    $(loginForm.password).blur(function(event){
         validate(isValidPassword(event.target.value), event.target);
     })
 
